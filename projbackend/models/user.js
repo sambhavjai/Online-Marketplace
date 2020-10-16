d58af072d23fecdc78129405fe70cf7a4e721cc0
sambhavjai <38015768+sambhavjai@users.noreply.github.com>
2020-10-16 15:35:38 +0530

    added user schema

---
+++ projbackend/models/user.js
@@ -0,0 +1,64 @@
+var mongoose=require('mongoose');
+var crypto=require('crypto');
+var uuid1=require('uuid/v1');
+
+var userSchema=new mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
+        maxlength: 32,
+        trim: true
+    },
+    lastname:{
+        type: String,
+        maxlength:32,
+        trim:true
+    },
+    email:{
+        type: String,
+        trim: true,
+        required: true,
+        unique: true
+    },
+    userInfo:{
+        type: String,
+        trim: true
+    },
+    encry_password:{
+        type: String,
+        required: true
+    },
+    salt: String,
+    role:{
+        type: Number,
+        default: 0
+    },
+    purchases:{
+        type: Array,
+        default: []
+    }
+});
+userSchema.virtual("password").get(function(){
+    return this._password;
+}).set(function(password){
+    this._password=password;
+    this.salt=uuidv1();
+    this.encry_password=this.securePassword(password);
+});
+
+userSchema.method={
+    authenticate: function(plainPassword){
+        return this.securePassword(plainPassword)===this.encry_password;
+    },
+    securePassword: function(plainPassword){
+        if(!password)
+        return "";
+        try{
+            return crypto.createHmac("sha256",this.salt).update(plainPassword).digest("hex");
+        }
+        catch(err){
+            return "";
+        }
+    }
+};
+module.exports=mongoose.model("User",userSchema);
