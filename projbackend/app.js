fbc6dfd383e0a5ac1fbd86c81c795f036599acfa
sambhavjai <38015768+sambhavjai@users.noreply.github.com>
2020-10-18 23:26:20 +0530

    added database connectivity

--- projbackend/app.js
+++ projbackend/app.js
@@ -1,1 +1,20 @@
-console.log("Hello World");
+require('dotenv').config();
+const mongoose=require('mongoose');
+const express=require('express');
+const app=express();
+
+mongoose.connect(process.env.DATABASE,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).then(()=>{
+    console.log("DB CONNECTED");
+}).catch(()=>{
+    console.log("DB ERROR");
+});
+
+const port=process.env.PORT||8000;
+
+app.listen(port,()=>{
+    console.log(`Server is up and running at port ${port}`);
+});
