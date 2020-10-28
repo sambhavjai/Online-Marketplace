3daf8858dcfdc94a0076d1daf21df27ac9b2f479
sambhavjai <38015768+sambhavjai@users.noreply.github.com>
2020-10-28 22:26:26 +0530

    added validation in signup

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -1,4 +1,6 @@
 const User=require("../models/user");
+const {check,validationResult} = require('express-validator');
+
 
 exports.signout=(req,res)=>{
     res.json({
@@ -7,6 +9,13 @@ exports.signout=(req,res)=>{
 };
 
 exports.signup=(req,res)=>{
+    const errors=validationResult(req);
+    if(!errors.isEmpty)
+    {
+        return res.status(422).json({
+            error: errors.array()[0].msg
+        })
+    }
     const user=new User(req.body);
     user.save((err,user)=>{
         if(err){
@@ -16,7 +25,11 @@ exports.signup=(req,res)=>{
         }
         else
         {
-            return res.json(user);
+            return res.json({
+                name: user.name,
+                email: user.email,
+                id: user._id
+            });
         }
     })    
 };
--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -1,8 +1,14 @@
 const express=require('express');
 const router=express.Router();
+const {check,validationResult} = require('express-validator');
 const {signout,signup}=require('../controllers/auth');
 
 router.get("/signout",signout);
-router.post("/signup",signup);
+
+router.post("/signup",[
+    check("name").isLength({min: 3}).withMessage("Name should be of atleast 3 chars"),
+    check("email").isEmail(),
+    check("password").isLength({min: 3})
+],signup);
 
 module.exports=router;
