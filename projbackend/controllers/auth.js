69cd47234dec8c1ac81118043fe5918b669095d6
sambhavjai <38015768+sambhavjai@users.noreply.github.com>
2020-10-31 15:18:18 +0530

    added data validation

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -10,7 +10,7 @@ exports.signout=(req,res)=>{
 
 exports.signup=(req,res)=>{
     const errors=validationResult(req);
-    if(!errors.isEmpty)
+    if(!errors.isEmpty())
     {
         return res.status(422).json({
             error: errors.array()[0].msg
--- projbackend/routes/auth.js
+++ projbackend/routes/auth.js
@@ -7,8 +7,8 @@ router.get("/signout",signout);
 
 router.post("/signup",[
     check("name").isLength({min: 3}).withMessage("Name should be of atleast 3 chars"),
-    check("email").isEmail(),
-    check("password").isLength({min: 3})
+    check("email").isEmail().withMessage("Please provide a correct email"),
+    check("password").isLength({min: 3}).withMessage("Password should of atleast 3 length")
 ],signup);
 
 module.exports=router;
