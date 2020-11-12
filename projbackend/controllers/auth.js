56bc39a1b7c9f734c45a21fc38368eae684fc3b8
sambhavjai <38015768+sambhavjai@users.noreply.github.com>
2020-11-12 22:05:35 +0530

    added authentication routes

--- projbackend/controllers/auth.js
+++ projbackend/controllers/auth.js
@@ -77,3 +77,24 @@ exports.isSignedIn = expressjwt({
     secret: process.env.SECRET,
     userProperty: "auth"
 });
+
+exports.isAuthenticated = (req,res,next) =>{
+    let checker = req.profile && req.auth && req.profile._id === req.auth._id;
+    if(!checker)
+    {
+        return res.status(403).json({
+            error : "Access Denied"
+        });
+    }
+    next();
+}
+
+exports.isAdmin = (req,res,next) =>{
+    if(req.profile.role === 0)
+    {
+        return res.status(403).json({
+            error: "You are not an Admin"
+        });
+    }
+    next();
+};
